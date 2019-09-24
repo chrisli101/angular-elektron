7df640f878e183984ed27cb6cb7c41f5f6ea4091
Christine Meier <cme@rit.at>
2019-09-24 16:07:20 +0200

    updates callback and state

--- main.ts
+++ main.ts
@@ -22,49 +22,49 @@ var clrMethod;
 // var dotNetFunction = edge.func('./../DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll');
 // dotNetFunction('test', function(error, result) { console.log('in dotnetf ' + result)})
 
-this.clrMethod = edge.func({
-  assemblyFile: './../../WebProjects/test/DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll',
-  typeName: 'StringUtility.StartUp',
-  methodName: 'test' // This must be Func<object,Task<object>>
-});
-this.clrMethod('test', function (error, result) {
-  // console.log(result);
-  test = result;
-}, false)
+// this.clrMethod = edge.func({
+//   assemblyFile: './../../WebProjects/test/DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll',
+//   typeName: 'StringUtility.StartUp',
+//   methodName: 'test' // This must be Func<object,Task<object>>
+// });
+// this.clrMethod('test', function (error, result) {
+//   // console.log(result);
+//   test = result;
+// }, false)
 
-var clrMethod2;
-
-
-
-this.clrMethod2 = edge.func({
-  assemblyFile: './../../WebProjects/test/DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll',
-  typeName: 'StringUtility.StartUp',
-  methodName: 'test2' // This must be Func<object,Task<object>>
-});
-this.clrMethod2(test, function (error, result) {
-  // console.log(result);
-  var test2 = result;
-}, false)
+// var clrMethod2;
 
 
 
-
-
-var start = edge.func({
-  assemblyFile: './../../WebProjects/test/DotNetCoreExample-ClassLibrary/SignLib/SignLib.dll',
-  typeName: signAgentProvider,
-  methodName: 'start'
-})
-start('test', function (error, result) {
-  console.log('signlib : ' + result);
-  var test = result;
-})
+// this.clrMethod2 = edge.func({
+//   assemblyFile: './../../WebProjects/test/DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll',
+//   typeName: 'StringUtility.StartUp',
+//   methodName: 'test2' // This must be Func<object,Task<object>>
+// });
+// this.clrMethod2(test, function (error, result) {
+//   // console.log(result);
+//   var test2 = result;
+// }, false)
 
 
 
 
-var certificates = [];
-var signaturealg = '';
+
+// var start = edge.func({
+//   assemblyFile: './../../WebProjects/test/DotNetCoreExample-ClassLibrary/SignLib/SignLib.dll',
+//   typeName: signAgentProvider,
+//   methodName: 'start'
+// })
+// start('test', function (error, result) {
+//   console.log('signlib : ' + result);
+//   var test = result;
+// })
+
+
+
+
+// var certificates = [];
+// var signaturealg = '';
 // var data = new Int32Array(32);
 // for (var i = 0; i < data.length; i++) {
 //   data[i] = 0x10;
@@ -83,16 +83,16 @@ var signaturealg = '';
 // console.log('BUFFER : XXXX ' + myBuffer);
 
 
-let hash = CryptoJS.SHA256('hello world');
-let buffer = Buffer.from(hash.toString(CryptoJS.enc.Hex), 'hex');
-let array = new Int32Array(buffer);
-// console.log('bytearray: ' + array.length);
+// let hash = CryptoJS.SHA256('hello world');
+// let buffer = Buffer.from(hash.toString(CryptoJS.enc.Hex), 'hex');
+// let array = new Int32Array(buffer);
+// // console.log('bytearray: ' + array.length);
 
-var getcertificate = edge.func({
-  assemblyFile: './../../WebProjects/test/DotNetCoreExample-ClassLibrary/SignLib/SignLib.dll',
-  typeName: signAgentProvider,
-  methodName: 'getCertificates'
-})
+// var getcertificate = edge.func({
+//   assemblyFile: './../../WebProjects/test/DotNetCoreExample-ClassLibrary/SignLib/SignLib.dll',
+//   typeName: signAgentProvider,
+//   methodName: 'getCertificates'
+// })
 // getcertificate('test', function (error, result) {
 //   console.log('signlib getCertificates: ' + result + ', error ' + error);
 //   certificates = result;
@@ -136,24 +136,24 @@ var getcertificate = edge.func({
 //   }*/
 // });
 
-var addAndMultiplyBy2 = edge.func({
-  assemblyFile: './../../WebProjects/test/DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll',
-  typeName: 'StringUtility.StartUp',
-  methodName: 'testCallBack'
-})
+// var addAndMultiplyBy2 = edge.func({
+//   assemblyFile: './../../WebProjects/test/DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll',
+//   typeName: 'StringUtility.StartUp',
+//   methodName: 'testCallBack'
+// })
 
-var payload = {
-  a: 2,
-  b: 3,
-  add: function (data, callback) {
-      callback(null, data.a + data.b);
-  }
-};
+// var payload = {
+//   a: 2,
+//   b: 3,
+//   add: function (data, callback) {
+//       callback(null, data.a + data.b);
+//   }
+// };
 
-addAndMultiplyBy2(payload, function (error, result) {
-  if (error) throw error;
-  console.log('callback return: ' + result);
-});
+// addAndMultiplyBy2(payload, function (error, result) {
+//   if (error) throw error;
+//   console.log('callback return: ' + result);
+// });
 
 //-----
 
@@ -174,29 +174,23 @@ ipcMain.on('gui-started', (event, arg) => {
       csCallback: function (data, callback) {
         console.log('CSharp callback called back');
         event.reply('nodeJsNotification','JUHUUUUUUU ' + data.a);
-        callback(null, 1);
+        callback(null, data.a);
       }
     },  
     function (error, result) {
       if (error) throw error;
-      event.reply('nodeJsNotification','HALLOOOOOOO');
+      event.reply('nodeJsNotification','HALLOOOOOOO' + result);
       // console.log('callback return: ' + result);
     });
   
 });
 
 
-// var createListener = edge.func(
-//   {
-//     source: function () {
-//     /*async (input) =>
-//     {
-//         var k = (int)input; 
-//         return (Func<object,Task<object>>)(async (i) => { return ++k; });}*/
-//     },
-//     references: [ './../../WebProjects/test/DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll' ]}
-
-// );
+var createListener = edge.func({
+  assemblyFile: './../../WebProjects/test/DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll',
+  typeName: 'StringUtility.StartUp',
+  methodName: 'holdState'
+})
 
 var createCounter = edge.func(function () {
   /*async (input) =>
@@ -209,9 +203,85 @@ var counter = createCounter(12, true); // create counter with 12 as initial stat
 // console.log(' XXXXXXXXXXXXXXXXXXXX' + counter(null, true)); // prints 13
 // console.log(counter(null, true)); // prints 14
 
+var listener = createListener(1, true);
+console.log(' Listener ' + listener(null, true)); 
+console.log(' Listener ' + listener(null, true)); 
+// console.log(listener(null, true)); // prints 14
+
+console.log('creating getMehods ');
+var getMehods = edge.func({
+  assemblyFile: './../../WebProjects/test/DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll',
+  typeName: 'StringUtility.StartUp',
+  methodName: 'getMehods'
+})
+
+var methods = getMehods(1, true);
+console.log('created getMehods ');
+console.log('full ' + methods.full);
+if (!!methods.full) {
+  console.log(Object.keys(methods.full));
+  Object.keys(methods.full).forEach(function (item) {
+    console.log(item); // key
+    console.log(methods.full[item]); // value
+  });
+  
+}
+console.log('getValue ' + methods.getValue);
+console.log('setValue ' + methods.setValue);
+
+console.log(' getMehods ' + methods.getValue("get1", true)); 
+console.log(' getMehods ' + methods.setValue("set1", true)); 
+console.log(' getMehods ' + methods.getValue("get2", true)); 
+console.log(' getMehods ' + methods.setValue("set2", true)); 
+console.log(' getMehods ' + methods.getValue("get3", true));
 
 
-//
+var getTest = edge.func(
+  // {
+  //   source: 
+    function () {
+    /*#r "./../../WebProjects/test/DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll"
+      #r "System.Data.dll"
+      
+        // using "./../../WebProjects/test/DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll";
+  
+                using System.Data;
+        //  using System;
+        //  using System.Net;
+         
+        //  using System.Collections.Generic;
+        //  using System.Threading;
+        using System.Threading.Tasks;
+        
+        using stringUtility;
+        public class Startup
+        {
+            public async Task<object> Invoke(object input)
+            {
+              var su = stringUtility();
+                // var su = new StartUp();
+                return new {
+                  getValue = "a",
+                  setValue = "b"
+                    // getValue = (Func<object, Task<object>>) su.getter, 
+                    // setValue = (Func<object, Task<object>>) su.setter
+                };
+            }
+    
+      }*/
+    }
+    // ,
+    // references: [ './../../WebProjects/test/DotNetCoreExample-ClassLibrary/stringUtility/bin/Debug/netstandard2.0/stringUtility.dll' ]
+  // }
+  );
+
+  console.log("Testing c# ind nodeJS");
+  var testMethods = getTest(12, true);
+  console.log(Object.keys(testMethods));
+  Object.keys(testMethods).forEach(function (item) {
+    console.log(item); // key
+    console.log(testMethods[item]); // value
+  });
 //Other regular electron main code
 //
 // Attach listener in the main process with the given ID
---
+++ stringUtility/StringLibrary.cs
@@ -0,0 +1,203 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using TestLib;
+
+namespace stringUtility
+{
+    
+  
+
+    public class StartUp
+    {
+        public string testMe = "init";
+        public Func<object, Task<object>> nodeCallback;
+        public Func<object, Task<object>> theGetter = null;
+
+        // public StartUp() {
+            
+        // }
+
+        public void setValue(String input)
+        {
+            testMe = input;
+        }
+
+        public string getValue()
+        {
+            return testMe;
+        }
+
+        public async Task<object> holdState(int input)
+        {
+            var k = (int)input;
+            var su = new stringUtility.StartUp();
+            return (Func<object, Task<object>>)(async (i) =>
+            {
+               
+                var currentValue = su.getValue(); 
+                Console.WriteLine("Current value: " + currentValue);
+                su.setValue(currentValue + "nochetwas");
+                Console.WriteLine("Changed value: " + su.getValue());
+                return ++k;
+            });
+        }
+
+        public async Task<object> getter(object val) {
+            Console.WriteLine("GET value: " + val);
+            return testMe;
+        }
+
+        public async Task<object> setter(object val) {
+            Console.WriteLine("SET value: " + val);
+            testMe = (string) val;
+            return val;
+        }
+
+        public async Task<object> getMehods(int input)
+        {
+            var k = (int)input;
+            var su = new stringUtility.StartUp();
+            return new {
+                getValue = (Func<object, Task<object>>) su.getter, 
+                setValue = (Func<object, Task<object>>) su.setter
+            };
+        }
+
+        public async Task<object> registerCallback(dynamic input)
+        {
+            nodeCallback = (Func<object, Task<object>>)input.csCallback;
+            var callbackArg = new { a = -1 };
+            await nodeCallback(callbackArg);
+
+            var t = Task.Factory.StartNew(async () =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    CancellationTokenSource source = new CancellationTokenSource();
+                    await Task.Delay(TimeSpan.FromSeconds(1.5), source.Token);
+                    await nodeCallback(new { a = i });
+                }
+
+            });
+
+            return 4;
+        }
+
+        public async Task<object> startListener(dynamic input)
+        {
+            nodeCallback = (Func<object, Task<object>>)input.csCallback;
+            var callbackArg = new { a = -1 };
+            await nodeCallback(callbackArg);
+
+            var t = Task.Factory.StartNew(async () =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    CancellationTokenSource source = new CancellationTokenSource();
+                    await Task.Delay(TimeSpan.FromSeconds(1.5), source.Token);
+                    await nodeCallback(new { a = i });
+                }
+            });
+
+            return 5;
+        }
+
+        public async Task<object> testCallBack(dynamic input)
+        {
+            var add = (Func<object, Task<object>>)input.add;
+            var twoNumbers = new { a = (int)input.a, b = (int)input.b };
+            var addResult = (int)await add(twoNumbers);
+            return addResult * 2;
+        }
+
+
+        public async Task<object> Invoke(String input)
+        {
+            Console.WriteLine("this.testMe " + this.testMe);
+            this.testMe = " already called and changed";
+            Console.WriteLine("this.testMe after " + this.testMe);
+            string ret = "hello there out " + input;
+            // Console.WriteLine("Sleep for 5 seconds.");
+            // Thread.Sleep(5000);
+            // Console.WriteLine("Slept.");
+            var testlib = new TestLib.TestClass();
+
+            await this.test2("test2");
+            return ret + " " + testlib.Test();
+        }
+        public async Task<object> test(String input)
+        {
+            Console.WriteLine("this.testMe " + this.testMe);
+            this.testMe = " already called and changed";
+            Console.WriteLine("this.testMe after " + this.testMe);
+            string ret = "hello there out " + input;
+            // Console.WriteLine("Sleep for 5 seconds.");
+            // Thread.Sleep(5000);
+            // Console.WriteLine("Slept.");
+            var testlib = new TestLib.TestClass();
+
+            await this.test2("test2");
+            return ret + " " + testlib.Test();
+        }
+        public async Task<object> test2(String input)
+        {
+            var testlib = new TestLib.TestClass();
+            Console.WriteLine("in test2 " + this.testMe);
+
+            return testlib.test2("test2");
+        }
+        // public async Task<object> StartMethode(object input)
+        // {
+        //     return ".NET begrüßt " + input.ToString();
+        // }
+
+        public async Task<object> StartsWithUpper(String str)
+        {
+            if (String.IsNullOrWhiteSpace(str))
+                return false;
+
+            Char ch = str[0];
+            return Char.IsUpper(ch);
+        }
+
+        public bool IsStringPalindrome(string str)
+        {
+            string revs = "";
+            for (int i = str.Length - 1; i >= 0; i--) //String Reverse
+            {
+                revs += str[i].ToString();
+            }
+            if (revs == str) // Checking whether string is palindrome or not
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    // public class Startup
+    // {
+    //     public async Task<object> Invoke(object input)
+    //     {
+    //         IDictionary<string, object> payload =
+    //             (IDictionary<string, object>)input;
+    //         int eineGanzZahl = (int)payload["eineGanzZahl"];
+    //         double eineGleitkommaZahl =
+    //             (double)payload["eineGleitkommaZahl"];
+    //         string einText = (string)payload["einText"];
+    //         bool einWahrheitWert = (bool)payload["einWahrheitWert"];
+    //         byte[] einPuffer = (byte[])payload["einPuffer"];
+    //         object[] eineArray = (object[])payload["einArray"];
+    //         IDictionary<string, object> einObjekt =
+    //             (IDictionary<string, object>)payload["einObjekt"];
+
+    //         return 1;
+    //     }
+    // }
+}
